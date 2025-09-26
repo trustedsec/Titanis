68138ee714549be6bf88a6486bf0433a0855c798
codewhisperer84 <codewhisp84@gmail.com>
2025-09-26 12:51:41 -0500

    IParameterGroup #530

---
+++ src/base/Titanis.Cli/IParameterGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titanis.Cli
+{
+	/// <summary>
+	/// Allows a parameter group to receive notifications during command execution.
+	/// </summary>
+	public interface IParameterGroup
+	{
+		void Initialize(Command command);
+	}
+}
