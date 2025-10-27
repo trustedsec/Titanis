6f40ce1053e6bbe173c9678eb6bc34a546e63e06
codewhisperer84 <codewhisp84@gmail.com>
2025-10-27 14:47:46 -0500

    Keytab

--- tools/security/Kerb/ListKeytabCommand.cs
+++ tools/security/Kerb/ListKeytabCommand.cs
@@ -6,19 +6,27 @@ namespace Kerb;
 
 [Command]
 [Description("Lists the entries in a keytab")]
+[OutputRecordType(typeof(KeytabEntry))]
 internal class ListKeytabCommand : Command
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-	[Parameter(0)]
+	[Parameter(0, EnvironmentVariable = "DEFCKTNAME")]
+	[Alias("kt")]
 	[Mandatory]
 	[Description("Name of keytab file")]
-	public string FileName { get; set; }
+	public string Keytab { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
-	protected sealed override Task<int> RunAsync(CancellationToken cancellationToken)
+	protected sealed override async Task<int> RunAsync(CancellationToken cancellationToken)
 	{
-		Keytab keytab = Keytab.LoadFrom(this.FileName);
+		this.WriteDiagnostic($"Loading keytab file '{this.Keytab}'");
+		KeytabFile keytab = KeytabFile.LoadFrom(this.Keytab);
 
-		throw new NotImplementedException();
+		foreach (var entry in keytab.Entries)
+		{
+			this.WriteRecord(entry);
+		}
+
+		return 0;
 	}
 }
