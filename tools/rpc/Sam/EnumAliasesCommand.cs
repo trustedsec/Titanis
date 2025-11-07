f3009c04dbd2ecebaaf4af75bc3738c5d147ebe8
codewhisperer84 <codewhisp84@gmail.com>
2025-11-07 12:34:41 -0600

    [Sam] enumaliases #681

---
+++ tools/rpc/Sam/EnumAliasesCommand.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titanis.Msrpc.Mssamr;
+using Titanis.Smb2.Cli;
+using Titanis.Winterop.Security;
+
+namespace Titanis.Cli.SamTool;
+
+public class UserInfo
+{
+	internal UserInfo(
+		SamEntry entry,
+		SecurityIdentifier sid,
+		string? domain,
+		SamUserGeneralInfo? general,
+		SamUserAccountInfo? accountInfo
+		)
+	{
+		Entry = entry;
+		Sid = sid;
+		Domain = domain;
+		General = general;
+		AccountInfo = accountInfo;
+	}
+
+	[Browsable(false)]
+	public SamEntry Entry { get; }
+	[Browsable(false)]
+	public SamUserGeneralInfo? General { get; }
+	[Browsable(false)]
+	public SamUserAccountInfo? AccountInfo { get; }
+
+	public string AccountName => this.Entry.Name;
+	public string Domain { get; }
+	public SamEntryType AccountType => this.Entry.EntryType;
+	public uint Id => this.Entry.Id;
+	public SecurityIdentifier Sid { get; }
+
+	// General
+	public string? FullName => this.General?.FullName;
+	public string? AdminComment => this.General?.AdminComment;
+
+	// Account
+	public DateTime? PasswordLastSet => this.AccountInfo?.PasswordLastSet;
+	public DateTime? LastLogon => this.AccountInfo?.LastLogon;
+	public int? BadPasswordCount => this.AccountInfo?.BadPasswordCount;
+}
+
+/// <task category="SAM;Enumeration">Enumerate user accounts in the Security Accounts Manager database</task>
+[Description("Enumerates user accounts")]
+[OutputRecordType(typeof(UserInfo))]
+[DetailedHelpText(@"{0} attempts to query the general and account info for the users returned by the server.")]
+[Example("Enumerate all accounts", "{0} LUMON-DC1 -UserName milchick -Password Br3@kr00m!")]
+internal sealed class EnumUsersCommand : SamDomainEnumCommand
+{
+	protected sealed override SamDomainAccess RequiredDomainAccess => SamDomainAccess.ListAccounts | SamDomainAccess.Read | SamDomainAccess.Lookup;
+	protected override async Task RunAsync(SamDomain domain, SamEntry domainInfo, Sam sam, CancellationToken cancellationToken)
+	{
+		List<SamEntry> entries;
+
+		// Users
+		try
+		{
+			this.WriteDiagnostic($"Enumerating users in domain '{domainInfo.Name}' ({domainInfo.Id}).");
+			entries = await domain.EnumUsers(cancellationToken);
+		}
+		catch (Exception ex)
+		{
+			if (this.ContinueOnError.IsSet)
+			{
+				this.WriteError($"Failed to enumerate users in domain '{domainInfo.Name}' with error: {ex.Message}");
+				return;
+			}
+			else
+				throw;
+		}
+
+		foreach (var entry in entries)
+		{
+			SamUser user;
+			try
+			{
+				user = await domain.OpenUserAsync(entry.Id, SamUserAccess.MaxAllowed, cancellationToken);
+			}
+			catch (Exception ex)
+			{
+				this.WriteError($"Failed to open user '{entry.Name}' with error: {ex.Message}");
+				continue;
+			}
+
+			using (user)
+			{
+				SamUserGeneralInfo? general = null;
+				SamUserAccountInfo? accountInfo = null;
+
+				try
+				{
+					general = await user.QueryGeneralInfo(cancellationToken);
+				}
+				catch (Exception ex)
+				{
+					this.WriteError($"Failed to query general info for user '{entry.Name}' with error: {ex.Message}");
+				}
+
+				try
+				{
+					accountInfo = await user.QueryAccountInfo(cancellationToken);
+				}
+				catch (Exception ex)
+				{
+					this.WriteError($"Failed to query account info for user '{entry.Name}' with error: {ex.Message}");
+				}
+
+				var userInfo = new UserInfo(entry, user.Sid, domainInfo.Name, general, accountInfo);
+
+				this.WriteRecord(userInfo);
+			}
+		}
+	}
+}
--- tools/rpc/Sam/Program.cs
+++ tools/rpc/Sam/Program.cs
@@ -6,6 +6,7 @@ namespace Titanis.Cli.SamTool;
 [Description("Commands for interacting with a remote Security Accounts Manager")]
 [Subcommand("enumusers", typeof(EnumUsersCommand))]
 [Subcommand("enumgroups", typeof(EnumGroupsCommand))]
+[Subcommand("enumaliases", typeof(EnumAliasesCommand))]
 internal class Program : MultiCommand
 {
 	static void Main(string[] args)
