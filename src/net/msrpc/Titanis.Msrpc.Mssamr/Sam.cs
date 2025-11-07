084568ba02e2e29be6a39461b5cdc47f196452be
codewhisperer84 <codewhisp84@gmail.com>
2025-11-07 12:23:09 -0600

    [Sam] OpenDomainAsync by SID

--- src/net/msrpc/Titanis.Msrpc.Mssamr/Sam.cs
+++ src/net/msrpc/Titanis.Msrpc.Mssamr/Sam.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Titanis.DceRpc;
+using Titanis.Winterop.Security;
 
 namespace Titanis.Msrpc.Mssamr
 {
@@ -16,8 +17,17 @@ namespace Titanis.Msrpc.Mssamr
 
 		public async Task<SamDomain> OpenDomainAsync(string name, SamDomainAccess access, CancellationToken cancellationToken)
 		{
+			ArgumentNullException.ThrowIfNull(name);
+
 			var domainSid = (await this._samClient.LookupDomain(this._handle, name, cancellationToken).ConfigureAwait(false)).value;
 			return await this._samClient.OpenDomain(this._handle, domainSid, access, cancellationToken).ConfigureAwait(false);
+		}
+
+		public async Task<SamDomain> OpenDomainAsync(SecurityIdentifier domainSid, SamDomainAccess access, CancellationToken cancellationToken)
+		{
+			ArgumentNullException.ThrowIfNull(domainSid);
+
+			return await this._samClient.OpenDomain(this._handle, domainSid.ToRpcSid(), access, cancellationToken).ConfigureAwait(false);
 		}
 
 		public Task<List<SamEntry>> GetDomains(CancellationToken cancellationToken)
