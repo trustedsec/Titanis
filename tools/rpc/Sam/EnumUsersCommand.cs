2d427b3a7383aaf70c0d5d4c9712d3bb74f9c313
codewhisperer84 <codewhisp84@gmail.com>
2025-11-07 12:33:47 -0600

    [Sam] Enumusers SID #680

--- tools/rpc/Sam/EnumUsersCommand.cs
+++ tools/rpc/Sam/EnumUsersCommand.cs
@@ -7,97 +7,99 @@ using System.Text;
 using System.Threading.Tasks;
 using Titanis.Msrpc.Mssamr;
 using Titanis.Smb2.Cli;
+using Titanis.Winterop.Security;
 
 namespace Titanis.Cli.SamTool;
 
-public class UserInfo
+public class AliasInfo
 {
-	internal UserInfo(
+	internal AliasInfo(
 		SamEntry entry,
-		SamUserGeneralInfo? general,
-		SamUserAccountInfo? accountInfo
+		SecurityIdentifier sid,
+		string? domain,
+		SamAliasGeneralInfo? general
 		)
 	{
 		Entry = entry;
+		Sid = sid;
+		Domain = domain;
 		General = general;
-		AccountInfo = accountInfo;
 	}
 
 	[Browsable(false)]
 	public SamEntry Entry { get; }
 	[Browsable(false)]
-	public SamUserGeneralInfo? General { get; }
-	[Browsable(false)]
-	public SamUserAccountInfo? AccountInfo { get; }
+	public SamAliasGeneralInfo? General { get; }
 
 	public string AccountName => this.Entry.Name;
+	public string Domain { get; }
+	public SamEntryType AccountType => this.Entry.EntryType;
 	public uint Id => this.Entry.Id;
+	public SecurityIdentifier Sid { get; }
 
 	// General
-	public string? FullName => this.General?.FullName;
+	public int? MemberCount => this.General?.MemberCount;
 	public string? AdminComment => this.General?.AdminComment;
-
-	// Account
-	public DateTime? PasswordLastSet => this.AccountInfo?.PasswordLastSet;
-	public DateTime? LastLogon => this.AccountInfo?.LastLogon;
-	public int? BadPasswordCount => this.AccountInfo?.BadPasswordCount;
 }
 
-/// <task category="SAM;Enumeration">Enumerate the accounts in the Security Accounts Manager database</task>
-[Description("Enumerates the users")]
-[OutputRecordType(typeof(UserInfo))]
-[DetailedHelpText(@"{0} attempts to query the general and account info for the users returned by the server.")]
-[Example("Enumerate all accounts", "{0} LUMON-DC1 -UserName milchick -Password Br3@kr00m!")]
-internal sealed class EnumUsersCommand : SamCommand
+/// <task category="SAM;Enumeration">Enumerate aliases in the Security Accounts Manager database</task>
+[Description("Enumerates aliases")]
+[OutputRecordType(typeof(AliasInfo))]
+[DetailedHelpText(@"{0} attempts to query the general info and attributes for the groups returned by the server.")]
+[Example("Enumerate all aliases", "{0} LUMON-DC1 -UserName milchick -Password Br3@kr00m!")]
+internal sealed class EnumAliasesCommand : SamDomainEnumCommand
 {
-	protected sealed override SamServerAccess RequiredSamAccess => SamServerAccess.EnumerateDomains | SamServerAccess.LookupDomain;
-
-	protected override async Task<int> RunAsync(Sam sam, CancellationToken cancellationToken)
+	protected sealed override SamDomainAccess RequiredDomainAccess => SamDomainAccess.ListAccounts | SamDomainAccess.Read | SamDomainAccess.Lookup;
+	protected override async Task RunAsync(SamDomain domain, SamEntry domainInfo, Sam sam, CancellationToken cancellationToken)
 	{
-		var domains = await sam.GetDomains(cancellationToken);
-		foreach (var domainInfo in domains)
+		List<SamEntry> entries;
+
+		try
 		{
+			this.WriteDiagnostic($"Enumerating aliases in domain '{domainInfo.Name}' ({domainInfo.Id}).");
+			entries = await domain.EnumAliases(cancellationToken);
+		}
+		catch (Exception ex)
+		{
+			if (this.ContinueOnError.IsSet)
+			{
+				this.WriteError($"Failed to enumerate aliases in domain '{domainInfo.Name}' with error: {ex.Message}");
+				return;
+			}
+			else
+				throw;
+		}
+
+		foreach (var entry in entries)
+		{
+			SamAlias alias;
 			try
 			{
-				var domain = await sam.OpenDomainAsync(domainInfo.Name, SamDomainAccess.ListAccounts | SamDomainAccess.Read | SamDomainAccess.Lookup, cancellationToken);
-				var users = await domain.EnumUsers(cancellationToken);
-				foreach (var entry in users)
-				{
-					try
-					{
-						using (var user = await domain.OpenUserAsync(entry.Id, SamUserAccess.MaxAllowed, cancellationToken))
-						{
-							SamUserGeneralInfo? general = null;
-							SamUserAccountInfo? accountInfo = null;
-
-							try
-							{
-								general = await user.QueryGeneralInfo(cancellationToken);
-							}
-							catch { }
-							try
-							{
-								accountInfo = await user.QueryAccountInfo(cancellationToken);
-							}
-							catch { }
-
-							var userInfo = new UserInfo(entry, general, accountInfo);
-
-							this.WriteRecord(userInfo);
-						}
-					}
-					catch (Exception ex)
-					{
-						this.WriteError($"Failed to open user '{entry.Name}' with error: {ex.Message}");
-					}
-				}
+				alias = await domain.OpenAliasAsync(entry.Id, SamAliasAccess.MaxAllowed, cancellationToken);
 			}
 			catch (Exception ex)
 			{
-				this.WriteError($"Failed to open domain '{domainInfo.Name}' with error: {ex.Message}");
+				this.WriteError($"Failed to open alias '{entry.Name}' with error: {ex.Message}");
+				continue;
+			}
+
+			using (alias)
+			{
+				SamAliasGeneralInfo? general = null;
+
+				try
+				{
+					general = await alias.QueryGeneralInfo(cancellationToken);
+				}
+				catch (Exception ex)
+				{
+					this.WriteError($"Failed to query general info for group '{entry.Name}' with error: {ex.Message}");
+				}
+
+				var groupInfo = new AliasInfo(entry, alias.Sid, domainInfo.Name, general);
+
+				this.WriteRecord(groupInfo);
 			}
 		}
-
-		return 0;
 	}
 }
---
+++ tools/rpc/Sam/SamDomainEnumCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titanis.Msrpc.Mssamr;
+using Titanis.Smb2.Cli;
+
+namespace Titanis.Cli.SamTool;
+internal abstract class SamDomainEnumCommand : SamCommand
+{
+	[Parameter]
+	[DefaultValue(true)]
+	public SwitchParam ContinueOnError { get; set; }
+
+	protected sealed override SamServerAccess RequiredSamAccess => SamServerAccess.EnumerateDomains | SamServerAccess.LookupDomain;
+
+	protected abstract SamDomainAccess RequiredDomainAccess { get; }
+
+	protected sealed override async Task<int> RunAsync(Sam sam, CancellationToken cancellationToken)
+	{
+		var domains = await sam.GetDomains(cancellationToken);
+		foreach (var domainInfo in domains)
+		{
+			SamDomain domain;
+			try
+			{
+				this.WriteDiagnostic($"Opening domain '{domainInfo.Name}' ({domainInfo.Id}).");
+				domain = await sam.OpenDomainAsync(domainInfo.Name, this.RequiredDomainAccess, cancellationToken);
+			}
+			catch (Exception ex)
+			{
+				if (this.ContinueOnError.IsSet)
+				{
+					this.WriteError($"Failed to open domain '{domainInfo.Name}' with error: {ex.Message}");
+					continue;
+				}
+				else
+					throw;
+			}
+
+			await this.RunAsync(domain, domainInfo, sam, cancellationToken);
+		}
+
+		return 0;
+	}
+
+	protected abstract Task RunAsync(SamDomain domain, SamEntry domainInfo, Sam sam, CancellationToken cancellationToken);
+}
