a950e1dcdbd0397932cb07603802e511e9bd2773
codewhisperer84 <codewhisp84@gmail.com>
2026-01-30 12:27:47 -0600

    [Wmi] doc

--- tools/rpc/Wmi/WmiObjectCommandBase.cs
+++ tools/rpc/Wmi/WmiObjectCommandBase.cs
@@ -23,7 +23,7 @@ internal abstract class WmiObjectCommandBase : WmiNamespaceCommandBase
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
 	[Parameter]
-	[Description("Continues processing when errors are encountered")]
+	[Description("Continue even if errors occur")]
 	public SwitchParam ContinueOnError { get; set; }
 
 	/// <summary>
