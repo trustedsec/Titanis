eba3cab4de2c03d6046ea395e3c9379246e47f60
codewhisperer84 <codewhisp84@gmail.com>
2026-02-16 09:11:25 -0600

    [Ldap] Select specific attributes for namedbits

--- tools/Ldap/NamedBitsCommand.cs
+++ tools/Ldap/NamedBitsCommand.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Text.RegularExpressions;
 using Titanis.Ldap;
 
 namespace Titanis.Cli.LdapTool;
@@ -8,9 +9,29 @@ namespace Titanis.Cli.LdapTool;
 [OutputRecordType(typeof(NamedBit))]
 internal class NamedBitsCommand : Command
 {
+	[Parameter(0)]
+	public string[]? Attribute { get; set; }
+
 	protected override Task<int> RunAsync(CancellationToken cancellationToken)
 	{
-		var all = NamedBitGroups.AllGroups.SelectMany(r => r.NamedBits, (g, r) => new { g, r }).Select(r => new NamedBit(r.g.AttributeName, r.r.Key, r.r.Value, $"0x{r.r.Value:X8}"));
+		IEnumerable<NamedBitGroup> groups = NamedBitGroups.AllGroups;
+		if (!this.Attribute.IsNullOrEmpty())
+		{
+			var attrGroups = (
+				from a in this.Attribute
+				join g in groups on a.ToUpper() equals g.AttributeName.ToUpper() into ag
+				select ag
+				);
+			foreach (var ga in attrGroups)
+			{
+				if (ga.Count() == 0)
+					this.WriteWarning($"Attribute {ga} didn't match any named bit groups.");
+			}
+
+			groups = attrGroups.SelectMany(g => g);
+		}
+
+		var all = groups.SelectMany(r => r.NamedBits, (g, r) => new { g, r }).Select(r => new NamedBit(r.g.AttributeName, r.r.Key, r.r.Value, $"0x{r.r.Value:X8}"));
 		this.WriteRecords(all);
 
 		return Task.FromResult<int>(0);
