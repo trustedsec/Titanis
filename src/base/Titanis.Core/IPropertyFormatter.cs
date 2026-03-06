27a74bc2a5d6fbb96f3a6332521db69e027a5089
codewhisperer84 <codewhisp84@gmail.com>
2026-03-06 13:04:49 -0600

    [Core] SaslUnsealTest

---
+++ src/base/Titanis.Core/IPropertyFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Titanis
+{
+	/// <summary>
+	/// Provides formatting for a property value.
+	/// </summary>
+	public interface IPropertyFormatter
+	{
+		string? Format(string format, object? value, PropertyDescriptor property);
+	}
+}
