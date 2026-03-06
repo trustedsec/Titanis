a66859d5df33b80dda86acf27ba7ba6941bac166
codewhisperer84 <codewhisp84@gmail.com>
2026-03-06 13:04:26 -0600

    [Asn1] BitCountAttribute

---
+++ src/formats/Titanis.Asn1/BitCountAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titanis.Asn1
+{
+	/// <summary>
+	/// Specifies the number of bits used in a bit string <see langword="enum"/>.
+	/// </summary>
+	/// <remarks>
+	/// This attribute is used by <see cref="Asn1BitString{TEnum}"/>.  The
+	/// bits start at the MSB and progress toward the LSB.
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.Enum)]
+	public sealed class BitCountAttribute : Attribute
+	{
+		public BitCountAttribute(int bitCount)
+		{
+			BitCount = bitCount;
+		}
+
+		/// <summary>
+		/// Gets the number of bits.
+		/// </summary>
+		public int BitCount { get; }
+	}
+}
