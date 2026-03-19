4fc5c4f1de7f7c36af48f1ab6b03b40035bd9bc8
codewhisperer84 <codewhisp84@gmail.com>
2026-03-19 14:48:35 -0500

    [Core} Duration #807

--- src/base/Titanis.Core/Duration.cs
+++ src/base/Titanis.Core/Duration.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Titanis
 {
@@ -21,10 +23,57 @@ namespace Titanis
 		}
 
 		public TimeSpan TimeSpan { get; }
+
+		public override string ToString()
+		{
+			var ts = this.TimeSpan;
+			if (ts == default)
+				return "0ms";
+
+			StringBuilder sb = new StringBuilder();
+			if (ts.Days > 0)
+				sb.Append($"{ts.Days}d");
+			if (ts.Hours > 0)
+				sb.Append($"{ts.Hours}h");
+			if (ts.Minutes > 0)
+				sb.Append($"{ts.Minutes}m");
+			if (ts.Seconds > 0)
+				sb.Append($"{ts.Seconds}s");
+			if (ts.Milliseconds > 0)
+				sb.Append($"{ts.Milliseconds}ms");
+
+			return sb.ToString();
+		}
+
+		private static readonly Regex rgxDuration = new Regex(@"^((?<d>\d+)d)?((?<h>\d+)h)?((?<m>\d+)m(?!s))?((?<s>\d+)s)?((?<ms>\d+)ms)?$");
+
+		public static Duration Parse(string text)
+		{
+			if (string.IsNullOrEmpty(text)) throw new ArgumentException($"'{nameof(text)}' cannot be null or empty.", nameof(text));
+
+			var m = rgxDuration.Match(text);
+			if (!m.Success)
+				throw new FormatException($"The value '{text}' does not indicate a duration.");
+
+			static int ParseIf(Match m, string group)
+			{
+				var g = m.Groups[group];
+				return g.Success ? int.Parse(g.Value) : 0;
+			}
+
+			int units = ParseIf(m, "d"); units *= 24;
+			units += ParseIf(m, "h"); units *= 60;
+			units += ParseIf(m, "m"); units *= 60;
+			units += ParseIf(m, "s"); units *= 1000;
+			units += ParseIf(m, "ms");
+
+			return new Duration(TimeSpan.FromMilliseconds(units));
+		}
 	}
 
-	class DurationConverter : TypeConverter
+	public class DurationConverter : TypeConverter
 	{
+
 		public sealed override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
 		{
 			return (sourceType == typeof(string));
@@ -32,24 +81,7 @@ namespace Titanis
 		public sealed override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
 		{
 			if (value is string str)
-			{
-				int mult = 0;
-				int cchSuffix = 0;
-				if (str.EndsWith("ms"))
-					(mult, cchSuffix) = (1, 2);
-				else if (str.EndsWith("s"))
-					(mult, cchSuffix) = (1000, 1);
-				else if (str.EndsWith("m"))
-					(mult, cchSuffix) = (60 * 1000, 1);
-				else if (str.EndsWith("h"))
-					(mult, cchSuffix) = (60 * 60 * 1000, 1);
-
-				if (mult == 0)
-					throw new FormatException($"The value '{str}' does not indicate the unit of time.");
-
-				double amount = double.Parse(str.Substring(0, str.Length - cchSuffix)) * mult;
-				return new Duration(TimeSpan.FromMilliseconds(amount));
-			}
+				return Duration.Parse(str);
 
 			return base.ConvertFrom(context, culture, value);
 		}
---
+++ test/base/Titanis.Core.Test/DurationTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titanis.Core.Test;
+
+[TestClass]
+public class DurationTests
+{
+	[TestMethod]
+	[DataRow("5ms", 5)]
+	[DataRow("5s", 5 * 1000)]
+	[DataRow("5m", 5 * 60 * 1000)]
+	[DataRow("5h", 5 * 60 * 60 * 1000)]
+	[DataRow("5d", 5 * 24 * 60 * 60 * 1000)]
+	[DataRow("5d4h3m2s1ms", (((5 * 24 + 4) * 60 + 3) * 60 + 2) * 1000 + 1)]
+	public void DurationTest(string text, int expectedMs)
+	{
+		var duration = Duration.Parse(text);
+		Assert.AreEqual(expectedMs, (int)duration.TimeSpan.TotalMilliseconds);
+		Assert.AreEqual(text, duration.ToString());
+	}
+}
