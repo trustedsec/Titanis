faa5bd4349fff0434fcb27afe074505f31d8b336
codewhisperer84 <codewhisp84@gmail.com>
2026-04-09 08:52:32 -0500

    [Test] Lsa whoami

---
+++ test/tools/Titanis.Cli.LsaTool.Test/WhoamiTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titanis.Cli.Kerb.Test;
+
+namespace Titanis.Cli.LsaTool.Test;
+
+[TestClass]
+public class WhoamiTests : CliCommandTest<WhoamiCommand>
+{
+	[TestMethod]
+	[CliTest("Ntlm_UserNamePassword", "Kerberos_UserNamePassword", "Kerberos_S4U", "Kerberos_Interrealm", "Kerberos_S4U_NtlmHash")]
+	public async Task TestWhoami(Token[] args)
+	{
+		var result = (await TestCommand(args))[0];
+	}
+}
--- tools/rpc/Lsa/WhoamiCommand.cs
+++ tools/rpc/Lsa/WhoamiCommand.cs
@@ -7,8 +7,12 @@ namespace Titanis.Cli.LsaTool;
 [Command]
 [OutputRecordType(typeof(UserPrincipalName), DefaultOutputStyle = OutputStyle.Freeform)]
 [Description("Gets the name and domain of the connected user")]
-[Example("Get connected user name", "{0} LUMON-FS1 -UserName milchick -Password Br3@kr00m!")]
-internal class WhoamiCommand : LsaCommand
+[Example("Get connected user name (NTLM)", "{0} LUMON-FS1 -UserName milchick -Password Br3@kr00m!", Tag = "Ntlm_UserNamePassword")]
+[Example("Get connected user name (Kerberos)", "{0} LUMON-FS1 -UserName milchick@LUMON -Password Br3@kr00m! -Kdc LUMON-DC1", Tag = "Kerberos_UserNamePassword")]
+[Example("Get connected user name (Kerberos S4U2proxy)", "{0} LUMON-FS1 -UserName allentown$@LUMON -Password password -S4UserName ColdHarbor -S4ProxyService host/allentown -Kdc LUMON-DC1", Tag = "Kerberos_S4U")]
+[Example("Get connected user name (Kerberos S4U2proxy with NTLM hash)", "{0} LUMON-FS1 -UserName allentown$@LUMON -NtlmHash 8846F7EAEE8FB117AD06BDD830B7586C -S4UserName ColdHarbor -S4ProxyService host/allentown -Kdc LUMON-DC1", Tag = "Kerberos_S4U_NtlmHash")]
+[Example("Get connected user name (Interrealm referral)", "{0} B5X-DC1.branch5x.lumon.ind -UserName milchick@LUMON -Password Br3@kr00m! -Kdc LUMON-DC1 -PreferSmb -EncryptRpc", Tag = "Kerberos_Interrealm")]
+public class WhoamiCommand : LsaCommand
 {
 	protected sealed override async Task<int> RunAsync(LsaClient client, CancellationToken cancellationToken)
 	{
